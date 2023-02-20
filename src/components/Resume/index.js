3b1df71cfd491b3891644910ab7234f36ee2eb94
Ema Halimi <ehalimi@na.edu>
2023-02-20 11:09:16 -0600

    updated my resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,7 +4,7 @@ function Resume() {
     return(
         <div className="resume">
             <h1>My Resume</h1>
-            <a href="https://docs.google.com/document/d/1iwZqhpdUheB69gacNqNmVLB7lwttCA9H1uldxcSY-h0/edit?usp=sharing" id="resume-link">View My Resume Here</a>
+            <a href="https://docs.google.com/document/d/116V3C2g9yzItXy3jAnjWPOSVSq7EYpLBSJmu4ZZonqg/edit" id="resume-link">View My Resume Here</a>
         </div>
     );
 }
